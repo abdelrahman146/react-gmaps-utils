060a08f2906d383f738e582bbcc9195254ce646b
Abdel Rahman <abdel.rahman@tradeling.com>
2024-02-21 16:40:44 +0400

    fixed a bug in tpyscript

--- src/Places/Autocomplete.tsx
+++ src/Places/Autocomplete.tsx
@@ -1,4 +1,11 @@
-import React, { useEffect, useRef, useState, useImperativeHandle, forwardRef } from 'react'
+import React, {
+  useEffect,
+  useRef,
+  useState,
+  useImperativeHandle,
+  forwardRef,
+  ElementType
+} from 'react'
 import { useGoogleMaps } from '../GoogleMapsContext'
 
 /**
@@ -8,83 +15,84 @@ import { useGoogleMaps } from '../GoogleMapsContext'
  * @returns {React.ReactNode} The Autocomplete component.
  */
 
-
-type AutocompleteInputProps = Omit<
-  React.HTMLProps<HTMLInputElement>,
-  'as'
->
-
-interface AutocompleteProps extends AutocompleteInputProps {
-  as?: React.ElementType | string
+type AutocompleteProps<C extends ElementType = 'input'> = {
+  as?: C
   onLoaded?: (autocomplete: google.maps.places.AutocompleteService) => void
   options?: Omit<google.maps.places.AutocompletionRequest, 'input'>
   renderResult?: (
     predictions: google.maps.places.AutocompletePrediction[]
   ) => React.ReactNode
-}
+} & Omit<React.ComponentPropsWithoutRef<C>, 'as'>
 
 export interface AutocompleteRef {
   close: () => void
 }
 
-export const Autocomplete = forwardRef<AutocompleteRef, AutocompleteProps>(({
-  as: Component = 'input',
-  onLoaded,
-  options,
-  renderResult,
-  value,
-  ...rest
-}: AutocompleteProps, ref) => {
-  const { scriptLoaded } = useGoogleMaps()
-  const autoComplete = useRef<google.maps.places.AutocompleteService | null>(
-    null
-  )
-  const [predictions, setPredictions] = useState<
-    google.maps.places.AutocompletePrediction[]
-  >([])
+export const Autocomplete = forwardRef(
+  <C extends ElementType = 'input'>(
+    {
+      as: Component = 'input' as C,
+      onLoaded,
+      options,
+      renderResult,
+      value,
+      ...rest
+    }: AutocompleteProps<C>,
+    ref: React.Ref<AutocompleteRef>
+  ) => {
+    const { scriptLoaded } = useGoogleMaps()
+    const autoComplete = useRef<google.maps.places.AutocompleteService | null>(
+      null
+    )
+    const [predictions, setPredictions] = useState<
+      google.maps.places.AutocompletePrediction[]
+    >([])
 
-  useEffect(() => {
-    if (scriptLoaded && !autoComplete.current) {
-      autoComplete.current = new google.maps.places.AutocompleteService()
-    }
-  }, [scriptLoaded])
+    useEffect(() => {
+      if (scriptLoaded && !autoComplete.current) {
+        autoComplete.current = new google.maps.places.AutocompleteService()
+      }
+    }, [scriptLoaded])
 
-  useEffect(() => {
-    if (autoComplete.current && value !== '') {
-      autoComplete.current.getPlacePredictions(
-        {
-          input: String(value),
-          ...options
-        },
-        (predictions) => {
-          if (predictions) {
-            setPredictions(predictions)
+    useEffect(() => {
+      if (autoComplete.current && value !== '') {
+        autoComplete.current.getPlacePredictions(
+          {
+            input: String(value),
+            ...options
+          },
+          (predictions) => {
+            if (predictions) {
+              setPredictions(predictions)
+            }
           }
-        }
-      )
-    } else {
-      setPredictions([])
-    }
-  }, [value])
+        )
+      } else {
+        setPredictions([])
+      }
+    }, [value])
 
-  useEffect(() => {
-    if (autoComplete.current && onLoaded) {
-      onLoaded(autoComplete.current)
-    }
-  }, [autoComplete.current, onLoaded])
+    useEffect(() => {
+      if (autoComplete.current && onLoaded) {
+        onLoaded(autoComplete.current)
+      }
+    }, [autoComplete.current, onLoaded])
 
-  useImperativeHandle(ref, () => ({
-    close: () => {
-      setPredictions([])
-    }
-  }))
+    useImperativeHandle(ref, () => ({
+      close: () => {
+        setPredictions([])
+      }
+    }))
 
-  return (
-    <React.Fragment>
-      <Component value={value} {...rest} />
-      {predictions.length > 0 && renderResult
-        ? renderResult(predictions)
-        : null}
-    </React.Fragment>
-  )
-})
+    return (
+      <React.Fragment>
+        <Component value={value} {...rest as any} />
+        {predictions.length > 0 && renderResult
+          ? renderResult(predictions)
+          : null}
+      </React.Fragment>
+    )
+  }
+) as <C extends ElementType>(
+  props: AutocompleteProps<C> & { ref?: React.Ref<AutocompleteRef> }
+) => JSX.Element
