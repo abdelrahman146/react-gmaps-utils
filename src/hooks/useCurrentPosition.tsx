581c7b110694aa22335f1f6f0f1bee310c009657
Abdel Rahman <abdel.rahman@tradeling.com>
2024-03-26 09:48:49 +0400

    feat: added get currentPosition based on IP address

--- src/hooks/useCurrentPosition.tsx
+++ src/hooks/useCurrentPosition.tsx
@@ -1,10 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
-type Position = google.maps.LatLngLiteral;
+type Position = google.maps.LatLngLiteral
 
 interface UseCurrentPositionOptions {
-  getPositionOnInit?: boolean;
+  onInit?: {
+    getPosition?: boolean
+    ipBased?: boolean
+  }
 }
+
+const cache: {ip?: Position, current?: Position} = {};
 
 /**
  * Custom hook that retrieves the current position of the user.
@@ -12,47 +17,77 @@ interface UseCurrentPositionOptions {
  * @returns An object containing the current position, loading state, error message, and a function to manually retrieve the current position.
  */
 export const useCurrentPosition = (options: UseCurrentPositionOptions = {}) => {
-  const [position, setPosition] = useState<Position | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-  const { getPositionOnInit = true } = options;
+  const [position, setPosition] = useState<Position | null>(null)
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<string | null>(null)
 
   const getCurrentPosition = () => {
-    setLoading(true);
-    setError(null);
-    
+    setLoading(true)
+    setError(null)
 
     const handlePosition = (position: GeolocationPosition) => {
-      const { latitude, longitude } = position.coords;
-      setPosition({ lat: latitude, lng: longitude });
-      setLoading(false);
-    };
+      const { latitude, longitude } = position.coords
+      setPosition({ lat: latitude, lng: longitude })
+      cache.current = { lat: latitude, lng: longitude };
+      setLoading(false)
+      setTimeout(() => { cache.current = undefined; }, 1000 * 60 * 10);
+    }
 
     const handleError = (error: GeolocationPositionError) => {
-      setError(error.message);
-      setPosition(null);
+      setError(error.message)
+      setPosition(null)
+      setLoading(false)
+    }
+
+    if (cache.current) {
+      setPosition(cache.current);
       setLoading(false);
-    };
+      return;
+    }
 
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(handlePosition, handleError);
+      navigator.geolocation.getCurrentPosition(handlePosition, handleError)
     } else {
       const error: GeolocationPositionError = {
         code: 2,
         message: 'Geolocation is not supported',
         PERMISSION_DENIED: 1,
         POSITION_UNAVAILABLE: 2,
-        TIMEOUT: 3,
-      };
-      handleError(error);
+        TIMEOUT: 3
+      }
+      handleError(error)
     }
-  };
+  }
+
+  const getIPBasedPosition = async () => {
+    setLoading(true)
+    setError(null)
+    if (cache.ip) {
+      setPosition(cache.ip);
+      setLoading(false);
+      return;
+    }
+    try {
+      const response = await fetch('https://ipapi.co/json/')
+      const data = await response.json()
+      const { latitude, longitude } = data
+      setPosition({ lat: parseFloat(latitude), lng: parseFloat(longitude) })
+      cache.ip = { lat: parseFloat(latitude), lng: parseFloat(longitude) };
+    } catch (error) {
+      setError(error.message)
+      setPosition(null)
+    } finally {
+      setLoading(false)
+    }
+  }
 
   useEffect(() => {
-    if(getPositionOnInit) {
-      getCurrentPosition();
+    if (options.onInit?.getPosition && !options.onInit?.ipBased) {
+      getCurrentPosition()
+    } else if (options.onInit?.getPosition && options.onInit?.ipBased) {
+      getIPBasedPosition()
     }
-  }, []);
+  }, [])
 
-  return { position, loading, error, getCurrentPosition };
-};
+  return { position, loading, error, getCurrentPosition, getIPBasedPosition }
+}
